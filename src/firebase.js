f839ea204e376b3c03a4969953bdeadbd4f22d5c
Luan Tran <devluant@gmail.com>
2024-09-05 15:27:22 +0700

    Use firebase keys via .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvdWwYA1me-z1NKAYl2NQtQjLQkL5EyIA",
-  authDomain: "deeplingos.firebaseapp.com",
-  projectId: "deeplingos",
-  storageBucket: "deeplingos.appspot.com",
-  messagingSenderId: "953311860861",
-  appId: "1:953311860861:web:6073b03985fe1c5c88f914",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
