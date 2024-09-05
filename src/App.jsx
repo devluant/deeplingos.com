bceea1febcd0442fc5ccdb918704507541eafe41
Luan Tran <devluant@gmail.com>
2024-09-05 15:51:34 +0700

    Create custom component: UploadMp3File

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-export default function App() {
+import React from "react";
+import UploadMP3File from "./components/UploadMp3File";
+
+export default function App() {            
     return (
-        ""
+        <>
+            <UploadMP3File />
+        </>
     )
 }
---
+++ src/components/UploadMp3File.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import hashText from "../utilities/hashText";
+import getLessonData from "../utilities/getLessonData";
+
+export default function UploadMP3File() {
+    const mp3InputRef = React.useRef(null)
+
+    function handleClick() {
+        mp3InputRef.current.click()
+    }
+
+    function handleChange(e) {
+        const file = e.target.files[0]
+
+        if (file) {
+            if (file.type === "audio/mpeg" || file.name.endsWith("mp3")) {
+                console.log(`${file.name}`)
+                hashText(file.name).then(hash => {
+                    console.log(hash)
+                    getLessonData(hash)
+                });
+            } else {
+                console.log("File type not valid. Only accept mp3 audio file.")
+                e.target.value = ""
+            }
+        }
+    }
+    return (
+        <>
+            <button onClick={ handleClick }>Upload MP3</button>
+            <input onChange={ handleChange } ref={mp3InputRef} type="file" id="mp3-input" accept=".mp3" style={ {display: "none"} } />
+        </>
+    )
+}
