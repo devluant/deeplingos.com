4552e288c3aceadfab914fc72c9b4a3798c8b93e
Luan Tran <devluant@gmail.com>
2024-09-05 13:58:08 +0700

    Setup firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvdWwYA1me-z1NKAYl2NQtQjLQkL5EyIA",
+  authDomain: "deeplingos.firebaseapp.com",
+  projectId: "deeplingos",
+  storageBucket: "deeplingos.appspot.com",
+  messagingSenderId: "953311860861",
+  appId: "1:953311860861:web:6073b03985fe1c5c88f914",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
