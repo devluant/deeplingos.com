281c29e5386fa61e7a8edacb867f7f12838ba62a
Luan Tran <devluant@gmail.com>
2024-09-08 06:49:48 +0700

    Remove import Text component

--- src/pages/Steps.jsx
+++ src/pages/Steps.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import Text from "../components/Text"
 import Dialogue from "../components/TextLineTypes/Dialogue"
 import Intro from "../components/TextLineTypes/Intro"
 import DialogueCTN from "../components/TextLineTypes/DialogueCTN"
