389d78e13499fbf540c1ea330019fe19bf19d534
Luan Tran <devluant@gmail.com>
2024-09-08 22:43:48 +0700

    Create Route to Export page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home";
 import Steps from "./pages/Steps";
+import Export from "./pages/Export";
 
 const AudioContext = React.createContext()
 
@@ -23,6 +24,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={ <Home lessonData={ lessonData } setLessonData={ setLessonData } /> } />
                     <Route path="/steps" element={ <Steps lessonData={ lessonData } lines={ lines } setLines={ setLines } audioFile={ audioFile } /> } />
+                    <Route path="/export" element={ <Export /> } />
                 </Routes>
             </AudioContext.Provider>
         </BrowserRouter>
